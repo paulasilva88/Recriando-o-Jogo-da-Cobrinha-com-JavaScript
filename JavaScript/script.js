326643db7c80eee1ee9e1d28f0910a661f6dc879
Paula Silva <paulageovana0808@gmail.com>
2021-04-03 17:13:54 -0300

    Relacionando o movimento da cobrinha com os botões do teclado

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,7 +6,7 @@ snake[0] = { /*Criando tamanho da cobrinha */
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right"; /*Dando direção à cobrinha */
+let direction = "left"; /*Dando direção à cobrinha */
 
 function criarFundoDoJogo() { /*Criando background onde o jogo ocorrerá */
     context.fillStyle = "lightgreen";
@@ -19,6 +19,16 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
     criarFundoDoJogo ()
     criarCobrinha()
@@ -27,9 +37,9 @@ function iniciarJogo(){
     let snakeY = snake[0].y;
 
     if(direction =="right") snakeX +=box;
-    if(direction =="left") snakeY -=box;
+    if(direction =="left") snakeX -=box;
     if(direction =="up") snakeY -=box;
-    if(direction =="down") snake +=box;
+    if(direction =="down") snakeY +=box;
 
     snake.pop(); /*Retira o último elemento da Array*/
 
