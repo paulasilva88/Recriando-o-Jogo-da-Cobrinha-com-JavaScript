50d3f67e3023c0aaab5cf9e791f1d8c56354bc2b
Paula Silva <paulageovana0808@gmail.com>
2021-04-08 14:34:00 -0300

    Criando comidinha

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,6 +7,10 @@ snake[0] = { /*Criando tamanho da cobrinha */
     y: 8 * box
 }
 let direction = "right"; /*Dando direção à cobrinha */
+let comidinha = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarFundoDoJogo() { /*Criando background onde o jogo ocorrerá */
     context.fillStyle = "lightpink";
@@ -19,6 +23,12 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+function desenharcomidinha(){
+    context.fillStyle = "blue";
+    context.fillRect(comidinha.x, comidinha.y, box, box)
+}
+
 /*Relacionando as teclas de direção do teclado co a direção da cobrinha*/
 document.addEventListener('keydown', update);
 
@@ -29,6 +39,7 @@ function update (event){
     /*37 botão para esquerda */
     if (event.keyCode == 39 && direction != "left") direction = "right";
     /*37 botão para esquerda */
+
     if (event.keyCode == 40 && direction != "up") direction = "down";
     /*37 botão para esquerda */
 }
@@ -43,6 +54,7 @@ function iniciarJogo(){
 
     criarFundoDoJogo ()
     criarCobrinha()
+    desenharcomidinha()
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
